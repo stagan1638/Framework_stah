63c68883f7eeff408fc4bc1fd316b98027df41fd
stagan1638 <61574992+stagan1638@users.noreply.github.com>
2021-01-05 01:35:47 +0300

    Update ItemTests.java

--- lab9/src/test/java/test/ItemTests.java
+++ lab9/src/test/java/test/ItemTests.java
@@ -3,6 +3,9 @@ package test;
 import model.Buyer;
 import model.Item;
 import org.hamcrest.core.Every;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import page.CatalogPage;
 import page.ItemPage;
@@ -16,6 +19,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class ItemTests extends CommonConditions {
+    /*@Test
+    public void testAddToCart() {
+        Item item = ItemCreator.forAddition();
+
+        Item cartItem = new CatalogPage(driver)
+                .openPage()
+                .closeCityPopup()
+                .addToCart(item)
+                .goToCart()
+                .waitForLoad()
+                .getFirstItem();
+
+        assertThat(cartItem, is(equalTo(item)));
+    }*/
+
     @Test
     public void testSearchItems() {
         String query = SearchQueryCreator.fromProperty();
@@ -28,3 +46,22 @@ public class ItemTests extends CommonConditions {
 
         assertThat(results, Every.everyItem(containsStringIgnoringCase(query)));
     }
+
+    /*@Test
+    public void testBuyQuickly() {
+        WebDriverWait waitForOne = new WebDriverWait(driver, 10);
+        waitForOne.until(ExpectedConditions.presenceOfElementLocated(By.xpath("some_xpath")));
+        Buyer buyer = BuyerCreator.forPurchase();
+        String expectedMessage = "Ваш заказ отправлен. Спасибо.";
+
+        ItemPage itemPage = new ItemPage(driver);
+        itemPage.openPage();
+        itemPage.closeCityPopup();
+        itemPage.showQuickPurchaseForm();
+        itemPage.buyQuickly(buyer);
+        String result = itemPage
+                .getBuyQuicklyResult();
+
+        assertThat(result, is(equalTo(expectedMessage)));
+    }*/
+}
